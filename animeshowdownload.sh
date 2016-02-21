4afd500b6d0e4789877d6859151a78a74fec806f
mohamedSamyMohsen <samy@sphinux.org>
2016-02-21 21:13:12 +0200

    Update animeshowdownload.sh

--- animeshowdownload.sh
+++ animeshowdownload.sh
@@ -21,6 +21,15 @@ echo "Getting video link..."
 
 videolink=`curl -s "$episodelink" | awk -F 'id="embbed-video"' '{print $2}' | awk -F 'SRC="' '{print $2}' | awk -F '"' '{print $1}' | tr -d '\n'`
 
-[[ -z "$outputdir" ]] && ./downloadMP4upload.sh "$videolink" && exit
+if [[ -n `echo $videolink | grep mp4upload` ]]
+then
+	[[ -z "$outputdir" ]] && ./downloadMP4upload.sh "$videolink" && exit
+	./downloadMP4upload.sh "$videolink" -d "$outputdir"
+fi
 
-./downloadMP4upload.sh "$videolink" -d "$outputdir"
+if [[ -n `echo $videolink | grep videonest` ]]
+then
+        [[ -z "$outputdir" ]] && ./downloadVideonest.sh "$videolink" && exit
+        ./downloadVideonest.sh "$videolink" -d "$outputdir"
+fi
+
